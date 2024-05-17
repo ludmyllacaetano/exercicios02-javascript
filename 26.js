ee2bb142d1e97dc4e67c8c1b31da935fbce1fe83
ludmylla <ludmyllacaetano1@gmail.com>
2024-05-17 18:29:53 -0300

    Correção do ex. 26.js

--- 26.js
+++ 26.js
@@ -18,20 +18,35 @@ function gerarMatrizAleatoria(numLinhas, numColunas){
 
 matrizA = gerarMatrizAleatoria(3, 5);
 console.log("Matriz A: ", matrizA);
-matrizB = gerarMatrizAleatoria(3, 5);
+matrizB = gerarMatrizAleatoria(5, 3);
 console.log("Matriz B: ", matrizB);
 
-function multiplicarMatriz(matrizA, matrizB){
-    let matrizProduto = [];
-    for(i = 0; i < 3; i++) {
-        let produto = [];
-        for (let j = 0; j < 5; j++) {
-            produto.push(matrizA[i][j] * matrizB[i][j]);
+// function multiplicarMatriz(matrizA, matrizB){
+//     let matrizProduto = [];
+//     for(i = 0; i < 3; i++) {
+//         let produto = [];
+//         for (let j = 0; j < 5; j++) {
+//             produto.push(matrizA[i][j] * matrizB[i][j]);
+//         }
+//         matrizProduto.push(produto);
+//     }
+//     return matrizProduto;
+// }
+
+function multiplicacaoDeMatrizes(matrizA, matrizB){
+    let produto = [];
+    for(let r = 0; r < matrizA.length; r++) {
+        produto[r] = [];
+        for(let c = 0; c < matrizB[0].length; c++) {
+            let soma = 0;
+            for(let i = 0; i < matrizA[0].length; i++) {
+                soma += matrizA[r][c] * matrizB[i][c];
+            }
+            produto[r][c] = soma;
         }
-        matrizProduto.push(produto);
     }
-    return matrizProduto;
+    return produto;
 }
 
-matrizProduto = multiplicarMatriz(matrizA, matrizB);
+matrizProduto = multiplicacaoDeMatrizes(matrizA, matrizB);
 console.log("A matriz produto é: ", matrizProduto);
